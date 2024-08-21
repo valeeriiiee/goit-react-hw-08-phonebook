5b9c4f2581e3088a34ac4e4502b213dd13b0da03
valeeriiiee <obleavalerie@gmail.com>
2024-08-21 23:39:48 +0800

    Create SharedLayout.jsx

---
+++ src/shared/SharedLayout.jsx
@@ -0,0 +1,10 @@
+import { Navigation } from './Navigation/Navigation';
+import React from 'react';
+
+export const SharedLayout = () => {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+};
