5efe9a59d988dc90061e665aec5002fde066f8ce
valeeriiiee <obleavalerie@gmail.com>
2024-08-22 00:22:27 +0800

    created pages

---
+++ src/pages/ContactsPage.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import { useSelector, useDispatch } from 'react-redux';
+import { setFilter } from '../redux/filterSlice';
+import { fetchContacts, addContact, deleteContact } from '../redux/operations';
+import {
+  selectFilter,
+  selectVisibleContacts,
+  selectIsLoading,
+  selectError,
+} from '../redux/selectors';
+
+export const App = () => {
+  const visibleContacts = useSelector(selectVisibleContacts);
+  const filter = useSelector(selectFilter);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  const handleAddContact = newContact => {
+    dispatch(addContact(newContact));
+  };
+
+  const handleDeleteContact = id => {
+    dispatch(deleteContact(id));
+  };
+
+  const handleSetFilter = newFilter => {
+    dispatch(setFilter(newFilter));
+  };
+
+  const filterContacts = contacts => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={handleAddContact} contacts={visibleContacts} />
+
+      <h2>Contacts</h2>
+      <Filter filter={filter} setFilter={handleSetFilter} />
+      {isLoading && (
+        <b style={{ display: 'block', padding: '0 0 20px 10px' }}>Loading...</b>
+      )}
+      {error && <b>Error: {error}</b>}
+      {visibleContacts && (
+        <ContactList
+          filterContact={filterContacts} // Function for filtering contacts
+          deleteContact={handleDeleteContact}
+          contacts={visibleContacts}
+        />
+      )}
+    </div>
+  );
+};
