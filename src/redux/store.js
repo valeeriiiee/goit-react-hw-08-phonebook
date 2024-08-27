a3b20d837dce1cde411b452cd702229c040b8d69
valeeriiiee <obleavalerie@gmail.com>
2024-08-27 21:14:47 +0800

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,28 +1,28 @@
 // store.js
-import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
+import { contactsSlice } from './contacts/contactsSlice';
+import { filterSlice } from './filter/filterSlice';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import filterReducer from './filterSlice';
-import contactsReducer from './contactsSlice';
-import { authSlice } from './authSlice';
+import { authReducer } from './auth/authSlice';
 
-const rootReducer = combineReducers({
-  auth: authSlice.reducer,
-  contacts: contactsReducer,
-  filter: filterReducer,
-});
-
-const persistConfig = {
-  key: 'root',
+// Persisting token field from auth slice to localstorage
+const authPersistConfig = {
+  key: 'auth',
   storage,
-  whitelist: ['contacts', 'token'],
+  whitelist: ['token'],
 };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    contacts: contactsSlice.reducer,
+    filter: filterSlice.reducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export const persistor = persistStore(store);
