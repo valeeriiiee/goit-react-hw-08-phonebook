c9e7a74f9ae9170c65e2186ac709c6d35c101974
valeeriiiee <obleavalerie@gmail.com>
2024-08-27 21:39:46 +0800

    Update RegisterPage.jsx

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,12 @@
+import { RegisterForm } from 'components/RegisterForm/RegisterForm';
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <title>Register</title>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default RegisterPage;
